73b030dbec8ff81776c3e7d4b9817b8a4c9e4342
Binank-1988 <binank1988@gmail.com>
2025-09-06 15:55:02 +0530

    Update kubeDeploy.groovy

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,5 +1,5 @@
 def call() {
-    withKubeConfig(credentialsId: 'k8s') {
+    withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
         sh "kubectl apply -f deployment.yml"
     }
 }
